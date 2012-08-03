6ba6e17a09fee46a2710406ac689def7e1b546a8
Dmytro Velychko <dmytro.velychko@gmail.com>
2012-08-03 15:11:23 +0200

    Finished VectorImageIntersect.m

--- Matlab/+Collision/IntersectionTest.m
+++ Matlab/+Collision/IntersectionTest.m
@@ -1,11 +1,35 @@
 function IntersectionTest()
+    close all;
+    %VectorGridIntersectTest();
+    VectorImageIntersect();
+end
+
+function VectorImageIntersect()
+    img = ones(20, 20, 20);
+    c = 5:15;
+    img(c, c, c) = 2;
+    c = 8:12;
+    img(c, c, c) = 3;
+    
+    pt1 = [10, 10, 10]';
+    pt2 = [10, 10, 20]';
+    [ptIntersectRes, valAtPt] = Collision.VectorImageIntersect(pt1, pt2, img);
+    figure;
+    plot(valAtPt);
+    pt2 = [10, 10, 0]';
+    [ptIntersectRes, valAtPt] = Collision.VectorImageIntersect(pt1, pt2, img);
+    figure;
+    plot(valAtPt);
+end
+
+function VectorGridIntersectTest()
     ptVec = [0.1, 0, 0,]';
     vVec = [1, 1, 1]';
     ptGrid = [0, 0, 0]';
     radGrid = 10;
     stepGrid = 1;
     ptIntersectX = Collision.VectorGridIntersect(ptVec, vVec, ptGrid, radGrid, stepGrid)
-    figure(1); 
+    figure; 
     plot3(ptIntersectX(1,:), ptIntersectX(2,:), ptIntersectX(3,:), '.');
     grid on;
 end
--- Matlab/+Collision/VectorImageIntersect.m
+++ Matlab/+Collision/VectorImageIntersect.m
@@ -1,3 +1,31 @@
-function ptIntersectX = VectorImageIntersect(ptVec, vVec, ptGrid, radGrid, stepGrid)
-
+function [ptIntersectRes, valAtPt] = VectorImageIntersect(pt1, pt2, img)
+    ptVec = pt1;
+    vVec = pt2-pt1;
+    ptGrid = [0, 0, 0]';
+    radGrid = max(size(img));
+    stepGrid = 1;
+    ptIntersectX = Collision.VectorGridIntersect(ptVec, vVec, ptGrid, radGrid, stepGrid);
+    sImg = size(img);
+    ptIntersectRes = nan(3, 0);
+    %valAtPt = nan(0, 0);
+    for k = 1:size(ptIntersectX, 2)
+        pt = ptIntersectX(:, k);        
+        for k2 = 1:3
+            if (vVec(k2) < 0)
+                pt(k2) = ceil(pt(k2));
+            else
+                pt(k2) = floor(pt(k2));
+            end
+        end
+        if (all(pt>=1) && all(pt<=sImg'))
+            ptIntersectRes = [ptIntersectRes, pt];
+        end
+    end
+    
+    nPt = size(ptIntersectRes, 2);
+    valAtPt = nan(1, nPt);
+    for k = 1:nPt
+        valAtPt(k) = img(ptIntersectRes(1,k), ptIntersectRes(2,k), ptIntersectRes(3,k));
+    end
+    
 end
