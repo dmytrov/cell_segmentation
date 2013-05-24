efff02c52198b28c9f81d6219127e44602c1e381
Dmytro Velychko <dmytro.velychko@gmail.com>
2013-05-24 12:13:37 +0200

    Another 'abstract' is removed

--- Matlab/+Bayesian/TPrior.m
+++ Matlab/+Bayesian/TPrior.m
@@ -2,7 +2,7 @@
 %   Dmytro Velychko - created. Euler AG, CIN, Tuebingen, 2012-2013
 %   mailto:dmytro.velychko@student.uni-tuebingen.de
 
-classdef (Abstract) TPrior < handle
+classdef TPrior < handle
     methods (Access = public)
         function obj = TPrior()
             % nothing to initialize here
